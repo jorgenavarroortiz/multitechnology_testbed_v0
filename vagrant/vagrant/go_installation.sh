2d7cabd3767ad0530600d83bcc25dcf8d815331f
Jorge <jorgenavarro@ugr.es>
2020-12-12 16:56:08 +0100

    Initial commit
    Initial commit

--- vagrant/vagrant/go_installation.sh
+++ vagrant/vagrant/go_installation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Go 1.12.9
+cd $HOME
 wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
 sudo tar -C /usr/local -zxvf go1.12.9.linux-amd64.tar.gz
 mkdir -p ~/go/{bin,pkg,src}
