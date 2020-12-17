f223e402ddd6edd39c3585a4ee909584fba572f3
Jorge <jorgenavarro@ugr.es>
2020-12-17 10:35:09 +0100

    NUC installation
    NUC installation

--- vagrant/vagrant/nuc_network1.sh
+++ vagrant/vagrant/nuc_network1.sh
@@ -2,7 +2,7 @@
 # Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
 
 # Intel Gigabit Ethernet Controller I219/V
-cp NUC/e1000e/e1000e-3.8.7.tar.gz $HOME
+cp NUC/e1000e-3.8.7.tar.gz $HOME
 cd $HOME
 tar zxf e1000e-3.8.7.tar.gz
 cd e1000e-3.8.7/src
