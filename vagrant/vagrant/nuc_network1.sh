609412603651a8b9ec5d3c3498f7d2a21c2f411f
Jorge <jorgenavarro@ugr.es>
2020-12-17 09:55:51 +0100

    NUC installation
    NUC installation

--- vagrant/vagrant/nuc_network1.sh
+++ vagrant/vagrant/nuc_network1.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
 
-sudo apt-get -y install wireless-tools dkms rfkill wpasupplicant
-
-# Intel Gigabit Ethernet Controller I219-V
+# Intel Gigabit Ethernet Controller I219/V
 cp NUC/e1000e/e1000e-3.8.7.tar.gz $HOME
 cd $HOME
 tar zxf e1000e-3.8.7.tar.gz
 cd e1000e-3.8.7/src
 sudo make install
 
-# Intel Wi-Fi 6 AX201
+sudo cp NUC/50-nuc.yaml.1 /etc/netplan/50-nuc.yaml
---
+++ vagrant/vagrant/nuc_network2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
+
+# Intel Wi-Fi 6 AX201 (already included in kernel>=5.2)
+sudo apt-get -y install wireless-tools dkms rfkill wpasupplicant
+
+# Copy netplan configuration for both Ethernet and Wi-Fi
+sudo cp NUC/ca.pem $HOME
+sudo cp NUC/50-nuc.yaml.2 /etc/netplan/50-nuc.yaml
