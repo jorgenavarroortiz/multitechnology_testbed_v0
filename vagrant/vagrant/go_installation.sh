81a61d8ca498f4952d492965b991e5316237c997
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2020-12-28 10:52:46 +0100

    Update go_installation.sh
    Bug fix

--- vagrant/vagrant/go_installation.sh
+++ vagrant/vagrant/go_installation.sh
@@ -5,7 +5,7 @@ cd $HOME
 wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
 sudo tar -C /usr/local -zxvf go1.12.9.linux-amd64.tar.gz
 mkdir -p ~/go/{bin,pkg,src}
-echo 'export GOPATH=$PATH/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin:$GOROOT/bin' >> ~/.bashrc
 echo 'export GO111MODULE=off' >> ~/.bashrc
