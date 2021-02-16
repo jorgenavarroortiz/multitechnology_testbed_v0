81f8689e10b599064725fa348b525688b499aa27
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2021-02-16 19:53:58 +0100

    Update nuc_network2.sh

--- vagrant/vagrant/nuc_network2.sh
+++ vagrant/vagrant/nuc_network2.sh
@@ -2,6 +2,8 @@
 # Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
 
 # Intel Wi-Fi 6 AX201 (already included in kernel>=5.2)
+# NOTE: This driver is unstable in kernel 5.4 (it crashes frequently). It does not work correctly in kernel 5.6. In latest kernels (5.8, 5.9 and 5.10) the throughput (using iperf) highly decreases for TCP connections (maybe due to some modification on the TCP parameters?).
+# As a conclusion, we will work using kernel 5.5, which has a robust driver and fast TCP connections. In addition, we have created a patch to include MPTCP functionality for this kernel.
 sudo apt-get -y install wireless-tools dkms rfkill wpasupplicant
 
 # Copy netplan configuration for both Ethernet and Wi-Fi
