f6fb94b1923c551769efb216a406bd256f9c0421
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2021-02-16 19:50:26 +0100

    No driver required in kernel v>=5.5
    Not required to install drivers for Intel Gigabit Ethernet Controller I219/V in latest kernels (v >= 5.5)

--- vagrant/vagrant/nuc_network1.sh
+++ vagrant/vagrant/nuc_network1.sh
@@ -2,10 +2,11 @@
 # Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
 
 # Intel Gigabit Ethernet Controller I219/V
-cp NUC/e1000e-3.8.7.tar.gz $HOME
-cd $HOME
-tar zxf e1000e-3.8.7.tar.gz
-cd e1000e-3.8.7/src
-sudo make install
+# Not required from kernel version >= 5.5 (commented here just in case)
+#cp NUC/e1000e-3.8.7.tar.gz $HOME
+#cd $HOME
+#tar zxf e1000e-3.8.7.tar.gz
+#cd e1000e-3.8.7/src
+#sudo make install
 
 sudo cp $HOME/vagrant/NUC/50-nuc.yaml.1 /etc/netplan/50-nuc.yaml
