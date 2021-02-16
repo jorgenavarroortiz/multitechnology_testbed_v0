5003932183fa0464e1f309b549da21ef3904f7f4
Jorge <jorgenavarro@ugr.es>
2021-02-16 19:47:51 +0100

    Kernel 5.5 with MPTCP support. GTP5G can be installed in kernels from version >= 5.2

--- vagrant/vagrant/free5gc_kernel_installation.sh
+++ vagrant/vagrant/free5gc_kernel_installation.sh
@@ -2,7 +2,9 @@
 # Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
 
 sudo apt-get -y update
-sudo apt-get -y install linux-image-5.0.0-23-generic
-sudo apt-get -y install fdutils linux-hwe-doc-5.0.0 linux-hwe-source-5.0.0 linux-hwe-tools linux-headers-5.0.0-23-generic
-sudo apt-get -y install fdutils linux-hwe-tools linux-headers-5.0.0-23-generic
-sudo apt-get -y install fdutils linux-headers-5.0.0-23-generic
+sudo apt-get -y install build-essential libncurses-dev bison flex
+
+cd $HOME/vagrant/MPTCP_kernel5.5
+sudo dpkg -i linux-image-5.5.0-mptcp_5.5.0-mptcp-1_amd64.deb
+sudo dpkg -i linux-headers-5.5.0-mptcp_5.5.0-mptcp-1_amd64.deb
+sudo dpkg -i linux-libc-dev_5.5.0-mptcp-1_amd64.deb
