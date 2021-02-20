ba661b6ffa39de6258ccd56662e79b1d1e5c4ce9
Jorge <jorgenavarro@ugr.es>
2021-02-20 15:57:12 +0100

    Using kernel 5.5 for MPTCP machines and kernel 5.0.0-23 for free5gc

--- vagrant/vagrant/free5gc_kernel_installation.sh
+++ vagrant/vagrant/free5gc_kernel_installation.sh
@@ -4,5 +4,10 @@
 sudo apt-get -y update
 sudo apt-get -y install build-essential libncurses-dev bison flex
 
-cd $HOME/vagrant/kernel5.0.19_generic
+cp $HOME/vagrant/gtp5g_working_with_kernel5.0.0-23.tar.gz $HOME
+cd $HOME
+tar xvfz gtp5g_working_with_kernel5.0.0-23.tar.gz
+mv gtp5g_working_with_kernel5.0.0-23 gtp5g
+
+cd $HOME/vagrant/kernel5.0.0-23
 sudo dpkg -i *.deb
