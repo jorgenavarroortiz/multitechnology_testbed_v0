415b117c09a41c523a33ffa957a32acc08298a8d
jorgenavarroortiz <jorgenavarro@ugr.es>
2021-03-18 12:47:54 +0100

    RPi4 support

---
+++ vagrant/vagrant/MPTCP_kernel5.5_RPi/mptcp_installation_kernel_rpi4.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# Jorge Navarro-Ortiz (jorgenavarro@ugr.es), 2021
+
+sudo dpkg -i linux-image-5.5.19-v8+_5.5.19-v8+-4_arm64.deb
+sudo dpkg -i linux-headers-5.5.19-v8+_5.5.19-v8+-4_arm64.deb
+sudo dpkg -i linux-libc-dev_5.5.19-v8+-4_arm64.deb
+tar xvfz boot_rpi_kernel55_mptcp.tar.gz
+cd boot
+sudo cp boot/* /boot/
+sudo cp boot/overlays/* /boot/overlays/
