b7bb5e2ed3d3d74cdf82fcb828a7b130ea33accc
jorgenavarroortiz <jorgenavarro@ugr.es>
2021-04-17 11:45:19 +0200

    Kernel 5.5 for RPi4 with MPTCP support and WRR v0.3 scheduler

---
+++ vagrant/vagrant/MPTCP_kernel5.5-WRR0.3_RPi4/mptcp_kernel_installation_rpi4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Jorge Navarro (jorgenavarro@ugr.es), Univerity of Granada
+
+sudo dpkg -i linux-image-5.5.19-mptcp-wrr03_5.5.19-mptcp-wrr03-1_arm64.deb
+sudo dpkg -i linux-headers-5.5.19-mptcp-wrr03_5.5.19-mptcp-wrr03-1_arm64.deb
+sudo dpkg -i linux-libc-dev_5.5.19-mptcp-wrr03-1_arm64.deb
+tar xvfz boot_rpi_kernel55_mptcp_wrr03.tar.gz
+sudo cp boot/* /boot/
+sudo cp boot/overlays/* /boot/overlays/
