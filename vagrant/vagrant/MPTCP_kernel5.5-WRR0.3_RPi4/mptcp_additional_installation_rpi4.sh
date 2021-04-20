c48300719a0880b7de17ff1aba4dd2583ec7569d
jorgenavarroortiz <jorgenavarro@ugr.es>
2021-04-20 07:57:44 +0200

    Updated support for RPi4 (MPTCP with WRR v0.3). Tested.

--- vagrant/vagrant/MPTCP_kernel5.5-WRR0.3_RPi4/mptcp_additional_installation_rpi4.sh
+++ vagrant/vagrant/MPTCP_kernel5.5-WRR0.3_RPi4/mptcp_additional_installation_rpi4.sh
@@ -6,7 +6,11 @@ sudo apt-get -y install git wget
 sudo apt-get -y install iperf ifstat
 sudo apt-get -y install build-essential libncurses-dev bison flex
 
+sudo apt-get -y install bridge-utils
+sudo apt-get -y install openvpn
+
 # iproute-mptcp (required for command "ip link set dev <interface> multipath <off/on/backup>")
+sudo apt-get -y install libmnl-dev libcap-dev
 cd $HOME
 git clone https://github.com/multipath-tcp/iproute-mptcp.git
 cd iproute-mptcp
