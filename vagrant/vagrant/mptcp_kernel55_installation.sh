e75645e6b368f15544238adc9edf462e5dea77e0
jorgenavarroortiz <jorgenavarro@ugr.es>
2021-06-07 22:42:14 +0200

    Update mptcp_kernel55_installation.sh

--- vagrant/vagrant/mptcp_kernel55_installation.sh
+++ vagrant/vagrant/mptcp_kernel55_installation.sh
@@ -5,8 +5,6 @@ sudo apt-get -y update
 sudo apt-get -y install build-essential libncurses-dev bison flex
 
 cd $HOME/vagrant/MPTCP_kernel5.5
-sudo dpkg -i linux-image-5.5.0-mptcp_5.5.0-mptcp-1_amd64.deb
-sudo dpkg -i linux-headers-5.5.0-mptcp_5.5.0-mptcp-1_amd64.deb
-sudo dpkg -i linux-libc-dev_5.5.0-mptcp-1_amd64.deb
+sudo dpkg -i *
 
 sudo apt-get -y install openvpn
