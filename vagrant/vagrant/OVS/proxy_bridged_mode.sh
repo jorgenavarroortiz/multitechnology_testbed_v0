e2602db74d604be0f20bdcdc3f01b977e2756c1a
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2021-06-29 22:20:59 +0200

    Update proxy_bridged_mode.sh

--- vagrant/vagrant/OVS/proxy_bridged_mode.sh
+++ vagrant/vagrant/OVS/proxy_bridged_mode.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Jorge Navarro-Ortiz (jorgenavarro@ugr.es), University of Granada, 2021
 
+sudo ifconfig toserver-br down 2> /dev/null
+sudo brctl delbr toserver-br 2> /dev/null
+
 #sudo ifconfig mtap0 0 promisc up
 sudo ifconfig eth2 0 promisc up
 sudo brctl addbr toserver-br
