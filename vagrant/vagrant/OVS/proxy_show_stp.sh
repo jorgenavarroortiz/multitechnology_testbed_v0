c9be0a2ee12d09c60b5782b6cc1a660d8be465be
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2022-05-14 12:48:25 +0200

    Add files via upload

--- vagrant/vagrant/OVS/proxy_show_stp.sh
+++ vagrant/vagrant/OVS/proxy_show_stp.sh
@@ -1,3 +1,8 @@
-sudo brctl showstp toserver-br
-sudo brctl showstp brmptcp_1
-sudo ip netns exec MPTCPns brctl showstp br_tap0
+#!/bin/bash
+
+EXISTS=`brctl show | grep toserver-br | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp toserver-br; fi
+EXISTS=`brctl show | grep brmptcp_1 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_1; fi
+EXISTS=`brctl show | grep br_tap0 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap0; fi
