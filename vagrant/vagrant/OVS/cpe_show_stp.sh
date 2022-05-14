46264b9737aa8797e4467c4ef3e832fa3220e39d
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2022-05-14 12:55:47 +0200

    Add files via upload

--- vagrant/vagrant/OVS/cpe_show_stp.sh
+++ vagrant/vagrant/OVS/cpe_show_stp.sh
@@ -1,8 +1,23 @@
+#!/bin/bash
+
 sudo ovs-appctl stp/show
 echo ""
-sudo brctl showstp brmptcp_1
+EXISTS=`brctl show | grep brmptcp_1 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_1; fi
 echo ""
-sudo brctl showstp brmptcp_2
+EXISTS=`brctl show | grep brmptcp_2 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_2; fi
 echo ""
-sudo brctl showstp brmptcp_3
+EXISTS=`brctl show | grep brmptcp_3 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_3; fi
 echo ""
+
+echo ""
+EXISTS=`sudo ip netns exec MPTCPns brctl show | grep br_tap0 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap0; fi
+echo ""
+EXISTS=`sudo ip netns exec MPTCPns brctl show | grep br_tap1 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap1; fi
+echo ""
+EXISTS=`sudo ip netns exec MPTCPns brctl show | grep br_tap2 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap2; fi
