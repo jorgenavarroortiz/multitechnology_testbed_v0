ab5567b0eca1749d021a186b3505a110a83b36a7
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2022-05-16 09:29:49 +0200

    Add files via upload

--- vagrant/vagrant/OVS/cpe_show_stp.sh
+++ vagrant/vagrant/OVS/cpe_show_stp.sh
@@ -3,21 +3,15 @@
 sudo ovs-appctl stp/show
 echo ""
 EXISTS=`brctl show | grep brmptcp_1 | wc -l`
-if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_1; fi
-echo ""
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_1; echo ""; fi
 EXISTS=`brctl show | grep brmptcp_2 | wc -l`
-if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_2; fi
-echo ""
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_2; echo ""; fi
 EXISTS=`brctl show | grep brmptcp_3 | wc -l`
-if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_3; fi
-echo ""
+if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_3; echo ""; fi
 
-echo ""
-EXISTS=`sudo ip netns exec MPTCPns brctl show | grep br_tap0 | wc -l`
-if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap0; fi
-echo ""
-EXISTS=`sudo ip netns exec MPTCPns brctl show | grep br_tap1 | wc -l`
-if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap1; fi
-echo ""
-EXISTS=`sudo ip netns exec MPTCPns brctl show | grep br_tap2 | wc -l`
+EXISTS=`sudo ip netns exec MPTCPns brctl show 2> /dev/null | grep br_tap0 | wc -l`
+if [[ $EXISTS == 1 ]]; then echo ""; sudo ip netns exec MPTCPns brctl showstp br_tap0; echo ""; fi
+EXISTS=`sudo ip netns exec MPTCPns brctl show 2> /dev/null | grep br_tap1 | wc -l`
+if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap1; echo ""; fi
+EXISTS=`sudo ip netns exec MPTCPns brctl show 2> /dev/null | grep br_tap2 | wc -l`
 if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap2; fi
--- vagrant/vagrant/OVS/proxy_show_stp.sh
+++ vagrant/vagrant/OVS/proxy_show_stp.sh
@@ -4,5 +4,5 @@ EXISTS=`brctl show | grep toserver-br | wc -l`
 if [[ $EXISTS == 1 ]]; then sudo brctl showstp toserver-br; fi
 EXISTS=`brctl show | grep brmptcp_1 | wc -l`
 if [[ $EXISTS == 1 ]]; then sudo brctl showstp brmptcp_1; fi
-EXISTS=`sudo ip net exec MPTCPns brctl show | grep br_tap0 | wc -l`
+EXISTS=`sudo ip net exec MPTCPns brctl show 2> /dev/null | grep br_tap0 | wc -l`
 if [[ $EXISTS == 1 ]]; then sudo ip netns exec MPTCPns brctl showstp br_tap0; fi
