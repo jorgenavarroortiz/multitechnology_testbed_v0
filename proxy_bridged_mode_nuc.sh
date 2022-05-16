3dd668edea1d5b13083b2e9f54376d8fcf527322
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2022-05-16 09:14:19 +0200

    Add files via upload

---
+++ proxy_bridged_mode_nuc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Jorge Navarro-Ortiz (jorgenavarro@ugr.es), University of Granada, 2021
+
+INTERFACETOSERVER='enp89s0'
+
+# Remove previous bridge
+sudo ifconfig toserver-br down 2> /dev/null
+sudo brctl delbr toserver-br 2> /dev/null
+
+# Create new bridge
+#sudo ifconfig mtap0 0 promisc up
+sudo ifconfig ${INTERFACETOSERVER} 0 promisc up
+sudo brctl addbr toserver-br
+sudo brctl addif toserver-br mtap0
+sudo brctl addif toserver-br ${INTERFACETOSERVER}
+sudo ifconfig toserver-br promisc up
+
+# Add STP to current bridges on proxy
+sudo brctl stp toserver-br on
+sudo brctl stp brmptcp_1 on
+sudo ip netns exec MPTCPns brctl stp br_tap0 on
